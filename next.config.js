5a0e20331af54d20e0bef2dc659e51b520607e0c
montassar attia <montassar.attia2@gmail.com>
2021-01-24 00:48:13 +0100

    dix run build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
+
+  // next.config.js
+/*
+ * Let's reuse the client configuration. I have rewritten
+ * it as a CommonJS module just to make it runs in a Node
+ * environment without any fuzz.
+ */
+
+
+
 module.exports = {
-    distDir: 'build',
-  }
+  // Make sure that your node enviroment supports async/await
+  exportPathMap: async function (defaultPathMap) {
   
+    const path = {
+      "/": { 
+        page: "/" 
+      }
+    }
+    return path
+  }
+}
