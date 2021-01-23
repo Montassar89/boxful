640be8596af1c91d4b96aff74138f60b3bb1ec08
montassar attia <montassar.attia2@gmail.com>
2021-01-24 00:39:48 +0100

    run build 6

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 // import Banner from './components/banner';
-// import Testimonials from './components/testimonials';
-// import ContactRow from './components/contact';
+// 
+
 
 export default function Home() {
   return (
