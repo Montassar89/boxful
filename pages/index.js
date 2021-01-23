8f6979c6d992da4f6e129d8070fb2fa84d955f96
montassar attia <montassar.attia2@gmail.com>
2021-01-24 00:31:42 +0100

    run build 6

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 // import Banner from './components/banner';
-import Testimonials from './components/testimonials';
-import ContactRow from './components/contact';
+// import Testimonials from './components/testimonials';
+// import ContactRow from './components/contact';
 
 export default function Home() {
   return (
     <div className="main">
       {/* <Banner /> */}
-      <Testimonials />
-      <ContactRow />
-    
+      {/* <Testimonials />
+      <ContactRow /> */}
+    red
     
     </div>
   )
