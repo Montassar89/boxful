bae424efb306281600db414a1d480d07c56fccc8
montassar attia <montassar.attia2@gmail.com>
2021-01-24 01:20:48 +0100

    responsive

--- pages/components/banner.js
+++ pages/components/banner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import home from '';
 const Banner = ({ title }) => {
-	
+
 	return (
 		<div>
 			<div className="w-full h-screen m-0 p-0 bg-cover bg-bottom" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1502921451607-29fa99d270d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80")`}}>
@@ -50,7 +50,7 @@ const Banner = ({ title }) => {
 										<div className="container  max-w-lg  flex flex-col space-y-4 justify-center items-center">
 
 
-											<div className="bg-transparent w-full flex items-center p-2 rounded-xl">
+											<div className="md:bg-transparent bg-white md:border-0 border w-full flex items-center p-2 rounded-xl">
 											   
 												<div className="flex-grow p-3">
 													<div className="text-sm text-gray-500 ">
@@ -163,7 +163,8 @@ const Banner = ({ title }) => {
 										<div className="container  max-w-lg  flex flex-col space-y-4 justify-center items-center">
 
 
-											<div className="w-full flex items-center p-2 rounded-xl  border border-gray-100">
+											<div className="md:bg-transparent bg-white md:border-0 border w-full flex 
+											items-center p-2 rounded-xl">
 
 												<div className="flex-grow p-3">
 													<div className="text-sm text-gray-500 ">
@@ -247,7 +248,7 @@ const Banner = ({ title }) => {
 														<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" className="bi bi-check2" viewBox="0 0 16 16">
 															<path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
 														</svg>
-														<p className="ml-2 text-sm text-black">Easy online booking & management</p>
+														<p className="ml-2 text-sm text-black">Easy online booking</p>
 													</div>
 												</div>
 												<a href="https://www.google.com/" className="flex items-center justify-center px-5 py-3 
