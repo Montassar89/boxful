ed47cf7609c5af7866c43596ff69110761ec6e37
montassar attia <montassar.attia2@gmail.com>
2021-01-28 00:27:05 +0100

    fix error

--- pages/layouts/header/DropMenuItem.js
+++ pages/layouts/header/DropMenuItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
-const DropMenuItem = ({items}) => {
+const DropMenuItem = (props) => {
 
     return (
         <ul className="bg-white shadow rounded mt-2 py-1 w-32 left-0 mt-40 -ml-4 absolute ">
             {
-                items.map((el, index) => {
-                    return (<li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-3 hover:bg-indigo-700 hover:text-white px-3 font-normal">{el}</li>
+                props.items.map((el, index) => {
+                    return (<li key={index} className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-3 hover:bg-indigo-700 hover:text-white px-3 font-normal">{el}</li>
                     )
                 })
             }
